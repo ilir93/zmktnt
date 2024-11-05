9991a128c0ad5b6d478a48487a767d3d792576e4
ilir93 <ilir@dua.com>
2024-11-05 13:33:04 +0100

    domain, sitemap

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const baseUrl = 'https://poezidashurieperty.com';
+const baseUrl = 'https://www.poezidashurieperty.com';
 const currentDate = new Date().toISOString().split('T')[0];
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
@@ -11,30 +11,6 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <lastmod>${currentDate}</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/poezi-dashurie</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.9</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/poezi-per-te-dashuren</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.9</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/vjersha-dashurie</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.9</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/poezi-dashurie-shqip</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.9</priority>
   </url>
 </urlset>`;
 
